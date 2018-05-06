eb76910a1e803de3ca3f6bf60fdbac7b50b48c4a
connono <1063496374@qq.com>
2018-05-06 19:56:20 +0800

    Add user status

---
+++ resources/views/shared/_status_form.blade.php
@@ -0,0 +1,6 @@
+<form method="post" action="{{ route('statuses.store') }}">
+    @include('shared._errors')
+    {{ csrf_field() }}
+    <textarea class="form-control" name="content" rows="3" placeholder="聊聊新鲜事儿...">{{ old('content') }}</textarea>
+    <button type="submit" class="btn btn-primary pull-right">发布</button>
+</form>
